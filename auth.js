16f1f327aafec11d7cadcab7479d3da0850225f4
SURUCHI GUPTA <109841435+Suruchi24Gupta@users.noreply.github.com>
2023-09-22 10:00:43 +0530

    middleware

--- auth.js
+++ auth.js
@@ -1,1 +1,24 @@
+const jwt = require('jsonwebtoken');
+const config = require('../config/jwt');
 
+function verifyToken(req, res, next) {
+    const token = req.header('x-auth-token');
+    
+    if (!token) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+    
+    try {
+        const decoded = jwt.verify(token, config.secret);
+        req.user = decoded.user;
+        next();
+    } catch (err) {
+        res.status(401).json({ message: 'Token is not valid' });
+    }
+}
+
+module.exports = verifyToken;
+
+/*successfully created the middleware/auth.js file with the authentication middleware code. 
+This middleware will be used to verify JWT tokens and protect routes that require authentication in your Node.js application. 
+You can continue building your application and use this middleware where needed for authentication purposes.*/
