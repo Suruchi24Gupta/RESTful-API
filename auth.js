74e25302361372e1f3455b3f3d39994086abf508
SURUCHI GUPTA <109841435+Suruchi24Gupta@users.noreply.github.com>
2023-09-22 10:03:52 +0530

    routes

--- auth.js
+++ auth.js
@@ -1,24 +1,94 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const config = require('../config/jwt');
+const User = require('../models/User');
 
-function verifyToken(req, res, next) {
-    const token = req.header('x-auth-token');
-    
-    if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
-    }
-    
+// Register a new user
+router.post('/register', async (req, res) => {
     try {
-        const decoded = jwt.verify(token, config.secret);
-        req.user = decoded.user;
-        next();
-    } catch (err) {
-        res.status(401).json({ message: 'Token is not valid' });
-    }
-}
+        const { username, password } = req.body;
+        
+        let user = await User.findOne({ username });
+        
+        if (user) {
+            return res.status(400).json({ message: 'User already exists' });
+        }
+        
+        user = new User({
+            username,  //name of any person who is registering
+            password,
+        });
+        
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+        
+        await user.save();
+        
+        const payload = {
+            
+            user: {
+                id: user.id,
+            },
+        };
+        
+        jwt.sign(
+            payload,
+            config.secret,
+            { expiresIn: config.expiresIn },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+            );
+        
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+});
 
-module.exports = verifyToken;
+// Login
+router.post('/login', async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        
+        const user = await User.findOne({ username });
+        
+        if (!user) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        
+        const isMatch = await bcrypt.compare(password, user.password);
+        
+        if (!isMatch) {
+            return res.status(400).json({ message: 'Invalid credentials' });
+        }
+        
+        const payload = {
+            user: {
+                id: user.id,
+            },
+        };
+        
+        jwt.sign(
+            payload,
+            config.secret,
+            { expiresIn: config.expiresIn },
+            (err, token) => {
+                if (err) throw err;
+                res.json({ token });
+            }
+            );
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server error');
+        }
+    });
 
-/*successfully created the middleware/auth.js file with the authentication middleware code. 
-This middleware will be used to verify JWT tokens and protect routes that require authentication in your Node.js application. 
-You can continue building your application and use this middleware where needed for authentication purposes.*/
+module.exports = router;
+
+/*successfully created the routes/auth.js file with the authentication routes for user registration and login. 
+These routes use the user model and JWT configuration you've previously defined.
+You can continue building your application and use these routes for user authentication.*/
